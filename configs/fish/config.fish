ec1c597249a7afa6116b7f5ea322d5d3c6bf9187
AsH131211 <aswanth.ash1312@gmail.com>
2026-02-07 16:07:51 +0530

    add user configs

---
+++ configs/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ash/.ghcup/bin $PATH # ghcup-env
